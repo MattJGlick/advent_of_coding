953a22d7b7aacd65773ef82dd28d13b72a9693b6
Matt Glick <matglick@amazon.com>
2024-12-01 11:52:51 -0500

    BACK AT IT. ~8 mins? 63316, 58621 (next day)

--- 2024/day_1/program.py
+++ 2024/day_1/program.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from itertools import combinations, permutations
+import heapq
 import numpy as np
 import re
 
@@ -8,4 +9,34 @@ from collections import defaultdict, Counter
 with open('input_file.txt') as inputfile:
     rows = [line.strip() for line in inputfile]
 
+left, right = [], []
 
+for row in rows:
+    f, s = row.split()
+    left.append(int(f))
+    right.append(int(s))
+heapq.heapify(left)
+heapq.heapify(right)
+
+total = 0
+while(left):
+    l = heapq.heappop(left)
+    r = heapq.heappop(right)
+    total += abs(l - r)
+
+print(total)
+
+### PART 2
+for row in rows:
+    f, s = row.split()
+    left.append(int(f))
+    right.append(int(s))
+
+count = defaultdict(int)
+for r in right:
+    count[r] += 1
+total = 0
+for l in left:
+    total += l * count[l]
+
+print(total)
